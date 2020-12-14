ed0dda86fd0dca3a62fcb763a7695d4cb81f2109
Cris Acosta <iamcris87@gmail.com>
2020-12-14 06:00:20 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react'
+import data from './utils/menu'
+import Categories from './components/Categories'
+import Menu from './components/Menu'
+
+const menuCategories = [ 'all', ...new Set(data.map(items => items.category))]
+
+function App() {
+
+  const [ menuItems, setMenuItems ] = useState(data)
+  const [ categories, setCategory ] = useState([])
+  const [ active, setActive ] = useState(false)
+
+  
+
+  const filterCategory = (category) => {
+    setActive(category)
+    const newItems = data.filter((item) => item.category === category)
+    return category === 'all' ? setMenuItems(data) : setMenuItems(newItems)
+  }
+
+  useEffect(()=> {
+    setCategory(menuCategories)
+  },[])
+
+  return (
+    <main >
+      <section className='menu section'>
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories filterCategory={filterCategory} categories={categories} active={active}/>
+        <Menu menu={menuItems}/>
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Categories.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Categories = ({ categories, active, filterCategory }) => {
+      
+    return (
+        <div className='btn-container'>
+            {
+                categories.map((category, index) => (
+                    <button key={index} className={`filter-btn ${active === category ? "active-btn" : null}`} onClick={()=> filterCategory(category)}>{category}</button>
+                ))
+            }
+        </div>
+    )
+}
+
+export default Categories
